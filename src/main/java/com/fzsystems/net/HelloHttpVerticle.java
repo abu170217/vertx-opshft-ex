e090a6b1b34ea8a2b4f593d7287b0b885a901974
Abu <abu170217@gmail.com>
2019-07-26 16:50:20 +0530

    Intial commit

---
+++ src/main/java/com/fzsystems/net/HelloHttpVerticle.java
@@ -0,0 +1,33 @@
+package com.fzsystems.net;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpHeaders;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.*;
+public class HelloHttpVerticle extends AbstractVerticle
+{
+	static final String HOSTNAME = System.getenv("HOSTNAME");
+	@Override
+	public void start() 
+	{
+		Router router = Router.router(vertx);
+		router.get("/").handler(this::hello);
+		router.get("/:name").handler(this::hello);
+		vertx.createHttpServer()
+		.requestHandler(router::accept)
+		.listen(8080);
+	}
+	
+	private void hello(RoutingContext rc) {
+		String message = "hello";
+		if (rc.pathParam("name") != null) 
+		{
+			message += " " + rc.pathParam("name");
+		}
+		JsonObject json = new JsonObject()
+				.put("message", message)
+				.put("served-by", HOSTNAME);
+		rc.response()
+		.putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+		.end(json.encode());
+	}
+}
